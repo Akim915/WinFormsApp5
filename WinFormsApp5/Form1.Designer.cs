08fef32848e8f96c50cb091c0275a664e3d84b3b
Akim <akim.marfe@op.pl>
2024-12-13 08:26:49 +0100

    Dodaj pliki projektów.

---
+++ WinFormsApp5/Form1.Designer.cs
@@ -0,0 +1,39 @@
+﻿
+namespace WinFormsApp5
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private Label lblCurrentPlayer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            lblCurrentPlayer = new Label();
+            SuspendLayout();
+           
+            lblCurrentPlayer.AutoSize = true;
+            lblCurrentPlayer.Font = new Font("Arial", 14F, FontStyle.Bold);
+            lblCurrentPlayer.Location = new Point(350, 150);
+            lblCurrentPlayer.Name = "lblCurrentPlayer";
+            lblCurrentPlayer.Size = new Size(197, 22);
+            lblCurrentPlayer.TabIndex = 0;
+            lblCurrentPlayer.Text = "Gracz X rozpoczyna";
+            
+            ClientSize = new Size(531, 400);
+            Controls.Add(lblCurrentPlayer);
+            Name = "Form1";
+            Text = "Kółko i Krzyżyk";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+    }
+}
---
+++ WinFormsApp5/Program.cs
@@ -0,0 +1,17 @@
+using WinFormsApp5;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormsApp5
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
